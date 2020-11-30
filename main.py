9dfd0bf32a421ba1427bd630c0133656311cca94
JackLL85 <75258441+JackLL85@users.noreply.github.com>
2020-11-30 08:54:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -28,3 +28,44 @@ class word():
                 line_start = (index*30+350,250)
                 line_end = (line_start[0]+20,250)
                 pygame.draw.line(screen,(255,255,255), line_start, line_end, 1)
+                
+                class VirtualKeyboard():
+    def __init__(self):
+        self.keys = ["Q","W","E","R","T","Y","U","I","O","P",
+                        "A","S","D","F","G","H","J","K","L",
+                         "Z","X","C","V","B","N","M","-"]
+
+        
+#making functions------------------------------------------------------------------------------------------------------
+def MouseInRect(x,y,w,h):
+    if pygame.mouse.get_pos()[0] > x and pygame.mouse.get_pos()[0] < x+w\
+       and pygame.mouse.get_pos()[1] > y and pygame.mouse.get_pos()[1] < y+h:
+        return True
+    else:
+        return False
+
+def ShowText(text, colour,x,y):
+    screen_text = FONT.render(text, True, colour)
+    screen.blit(screen_text,[x,y])
+
+def KeyboardInput():
+
+    value_returned = False
+    
+    for letter in range(10):
+        if MouseInRect((75*letter)+50,300,35,35):
+            return vk.keys[letter]
+            value_returned = True
+
+    for letter in range(9):
+        if MouseInRect((75*letter)+100,350,35,35):
+            return vk.keys[letter+10]
+            value_returned = True
+            
+    for letter in range(8):
+        if MouseInRect((75*letter)+150,400,35,35):
+            return vk.keys[letter+19]
+            value_returned = True
+            
+    if not value_returned:
+        return False
