3a360ee817d908c751b97d81229ab97e09608fe3
Milos Olguin <65838439+1000os@users.noreply.github.com>
2020-11-30 08:52:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,25 @@ pygame.init()
 WIDTH, HEIGHT = 800,500
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Hangman")
+#making classes-------------------------------------------------------------------------------------------------------------
+class word():
+    def __init__(self, word):
+        self.string = word
+        self.word_len = len(self.string)
+        self.letters = []
+        
+        for index, letter in enumerate(self.string):
+            self.letters.append(self.string[index])
+        print(self.letters)
+    def draw(self):
+        
+        for index in range(len(self.letters)):
+            if self.letters[index] == self.letters[index].upper():
+                if index == 0:
+                    ShowText(self.letters[index],(255,255,255),index*30+350,220)
+                else:
+                    ShowText(self.letters[index].lower(),(255,255,255),index*30+350,220)
+            else:
+                line_start = (index*30+350,250)
+                line_end = (line_start[0]+20,250)
+                pygame.draw.line(screen,(255,255,255), line_start, line_end, 1)
