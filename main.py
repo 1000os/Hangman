197b299181191c32893a76f5f0ecf81b78551c39
Milos Olguin <65838439+1000os@users.noreply.github.com>
2020-11-30 08:51:28 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import pygame
+import random
+
+#setting up the screen------------------------------------------------------------------------------------------------------
+pygame.init()
+WIDTH, HEIGHT = 800,500
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Hangman")
