5789dc29745fd674f7d42187d4335f3072266cd0
Milos Olguin <65838439+1000os@users.noreply.github.com>
2020-11-30 08:56:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -48,8 +48,9 @@ def ShowText(text, colour,x,y):
     screen_text = FONT.render(text, True, colour)
     screen.blit(screen_text,[x,y])
 
+    
+    
 def KeyboardInput():
-
     value_returned = False
     
     for letter in range(10):
@@ -69,3 +70,50 @@ def KeyboardInput():
             
     if not value_returned:
         return False
+    
+    
+    
+def DrawKeyboard():    
+    for x in range(10):
+        pygame.draw.ellipse(screen,(255,255,255),[(75*x)+50,300,35,35])
+    for x in range(9):
+        pygame.draw.ellipse(screen,(255,255,255),[(75*x)+100,350,35,35])
+    for x in range(8):
+        pygame.draw.ellipse(screen,(255,255,255),[(75*x)+150,400,35,35])
+    
+    for x in range(10):
+        ShowText(vk.keys[x],(0,0,0),(75*x)+55,295)
+    for x in range(9): 
+        ShowText(vk.keys[x+10],(0,0,0),(75*x)+105,345)
+    for x in range(8):
+        ShowText(vk.keys[x+19],(0,0,0),(75*x)+155,395)
+
+def DrawEverything():
+    screen.fill((50,50,50))
+    DrawKeyboard()
+    w.draw()
+    if attempts <= 6:
+        screen.blit(images[hangman_status],(50,50))
+    ShowText("Attempts left: " + str(len(images)-attempts),(255,255,255),650,30)
+
+#loading the images----------------------------------------------------------------------------------------------------------------------------------------------------------------
+images = []
+for i in range(7):
+    image = pygame.image.load("hangman"+str(i)+".png")
+    images.append(image)
+
+#game variables------------------------------------------------------------------------------------------------------------------------------------------------------------
+hangman_status = 0
+f = open('WordList.txt','r')
+content = f.read()
+f.close()
+print(type(content))
+WORD_LIST = content.split(",")
+word_string = random.choice(WORD_LIST)
+print(word_string)
+FONT = pygame.font.Font("Pacifico.ttf",20)
+w = word(word_string)
+vk = VirtualKeyboard()
+gs = 1
+global attempts
+attempts = 0
